9e4d949c5a0488782901b3d2af83e732cd6782f0
Jaanika Muurisepp <jaanika.muurisepp@gmail.com>
2017-05-11 20:21:04 +0300

    Masina andmed

---
+++ andmed.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Andmed masina kohta
+#set -x
+clear
+
+# Machine name
+printf "Machine name:		" && hostname
+
+# Full hostname
+# "Full hostname:		" && $(hostname).$(domainname)
+
+# IP address
+printf "IP address:		" && ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | awk 'FNR>=1 && FNR<=1'
+
+# MAC address
+printf "MAC address:		" && cat /sys/class/net/eth0/address
+
+# OS architecture
+printf "OS architecture:        " && dpkg --print-architecture
+
+# Kernel version
+printf "Kernel version:		" && uname -v
+
+# CPU name
+printf "CPU name:		" && cat /proc/cpuinfo | grep 'model name' | awk '{print $4, $5,$6,$7,$8}'
+
+# CPU cores
+printf "CPU cores:		" && cat /proc/cpuinfo | grep cores | awk '{print $4}'
+
+# Memory
+printf "Memory:			" && free -m -h | awk '{print $2}' | awk 'FNR>=2 && FNR<=2'
+
+# Hard disk space
+printf "Hard disk space:	" && df -hT /home | awk '{print $3}' | awk 'FNR>=2 && FNR <=2'
+
+# Linux distro valjastamine
+printf "Distro:			" && lsb_release -d | awk '{print $2,$3,$4,$5}'
+
+# exit
